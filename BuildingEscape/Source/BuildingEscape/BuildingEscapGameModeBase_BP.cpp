2a62ffac8716eb6f8ce033aeac8ab70a517b3cca
DESKTOP-15JD85D\benji <benji.b.ramirez@gmail.com>
2017-03-09 21:13:11 -0800

    began adding ray stuff

---
+++ BuildingEscape/Source/BuildingEscape/BuildingEscapGameModeBase_BP.cpp
@@ -0,0 +1,8 @@
+// Copyright Ben Ramirez 2017
+
+#include "BuildingEscape.h"
+#include "BuildingEscapGameModeBase_BP.h"
+
+
+
+
---
+++ BuildingEscape/Source/BuildingEscape/BuildingEscapGameModeBase_BP.h
@@ -0,0 +1,19 @@
+// Copyright Ben Ramirez 2017
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapGameModeBase_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPE_API ABuildingEscapGameModeBase_BP : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
